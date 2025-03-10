3f10c6a3e31f92031b5c578b1844669d47ec2c0c
4Byteint <jingwenh476@gmail.com>
2025-03-10 21:50:13 +0800

    做透視變換

--- PerspectiveTransform.py
+++ PerspectiveTransform.py
@@ -1,33 +1,33 @@
+# -*- coding:utf-8 -*-
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-# 讀取圖片
-image = cv2.imread("./calibration/img14_cali.png")
+# 计算透视变换参数矩阵
+def cal_perspective_params(img, points):
+    height, width = img.shape[:2]
 
-# 定義四個已知點 (變形前的四個角落)
-src_points = np.array([(147, 0), (492, 0), (468, 360), (197, 360)], dtype=np.float32)
+    # src 和 dst 對應四個點，用來計算透視變換矩陣
+    src = np.float32(points)
+    width_top = np.linalg.norm(points[1] - points[0])  
+    width_bottom = np.linalg.norm(points[2] - points[3])  
+    width = max(int(width_top), int(width_bottom))  
+    height_left = np.linalg.norm(points[3] - points[0])  
+    height_right = np.linalg.norm(points[2] - points[1])  
+    height = max(int(height_left), int(height_right))  
 
-# 計算寬度 (取上邊與下邊的平均長度)
-width_top = np.linalg.norm(src_points[0] - src_points[1])  # 左上到右上
-width_bottom = np.linalg.norm(src_points[2] - src_points[3])  # 右下到左下
-width = int(max(width_top, width_bottom))  # 選擇較大者作為寬度
+    dst = np.float32([[0,0],[width,0],[width,height],[0,height]])
+    # 透视矩阵
+    M = cv2.getPerspectiveTransform(src, dst)
+    print(M)
+    warped_img = cv2.warpPerspective(img, M, (width, height))
+    return M, warped_img
 
-# 計算高度 (取左邊與右邊的平均長度)
-height_left = np.linalg.norm(src_points[0] - src_points[3])  # 左上到左下
-height_right = np.linalg.norm(src_points[1] - src_points[2])  # 右上到右下
-height = int(max(height_left, height_right))  # 選擇較大者作為高度
-
-# 定義變換後的矩形座標
-dst_points = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
-
-# 計算透視變換矩陣
-matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-
-# 進行透視變換
-warped_image = cv2.warpPerspective(image, matrix, (width, height))
-
-# 顯示結果
-cv2.imshow("Original Image", image)
-cv2.imshow("Warped Image", warped_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    img = cv2.imread('./imprint/al/img0.png')
+    points = np.array([(147,0),(492,0),(458,360),(197,345)])
+    M, transform_img = cal_perspective_params(img, points)
+    cv2.imshow('img',img)
+    cv2.imshow('trasform_img',transform_img)
+    cv2.imwrite('trasform_img0.png',transform_img)
+    cv2.waitKey(0)
