68382e1a4ac979609a64da5c2f2750b5b0de3f14
4Byteint <jingwenh476@gmail.com>
2025-03-15 12:50:24 +0800

    delete

--- calibration.py
+++ calibration.py
@@ -10,7 +10,7 @@ objp[:,:2] = np.mgrid[0:chessboard[0],0:chessboard[1]].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('./calibration/cam/*.png')
+images = glob.glob('./calibration/fixed_cam/*.png')
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -44,10 +44,6 @@ newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 0.9, (w, h))
 
 # 讀取待校正的圖像
 img = cv.imread('./calibration/fixed_cam/img1.png')
-dst = cv.remap(img, mapx, mapy,cv.INTER_LINEAR)
-# 進行去畸變校正
-#dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-# 儲存校正後的影像
-cv.imwrite('calibresult.png', undistorted_img)
-
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+cv.imwrite("calibresult.png", dst)
 print("Calibration completed successfully. Output saved as calibresult.png")
