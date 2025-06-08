65a2b1d97a4f35e2c95ed618a8c2e2f739a3134a
4Byteint <jingwenh476@gmail.com>
2025-06-09 00:48:22 +0800

    training...

--- auto_project/train_segmentation.py
+++ auto_project/train_segmentation.py
@@ -198,8 +198,8 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("🚀 使用設備：", device)
 
-    image_dir = "./imprint/dataset.label/final_dataset_voc/PNGImages"
-    mask_dir = "./imprint/dataset.label/final_dataset_voc/SegmentationClass"
+    image_dir = "./dataset/v1/data_dataset_voc/PNGImages"
+    mask_dir = "./dataset/v1/data_dataset_voc/SegmentationClass"
 
     dataset = SegmentationDataset(image_dir, mask_dir)
     train_len = int(0.8 * len(dataset))
